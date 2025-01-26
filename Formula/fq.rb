b671bc79066e9208ec24365989eb472bb8a3df02
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-01-26 13:13:44 +0100

    feat: fq 1.2.3 (new formula)

---
+++ Formula/fq.rb
@@ -0,0 +1,19 @@
+class Fq < Formula
+  desc "A command-line interface (CLI) tool to interact with Firestore"
+  homepage "https://github.com/steschwa/fq"
+  url "https://github.com/steschwa/fq/archive/v1.2.3.tar.gz"
+  sha256 "11552de7615bd092e24063c492531f998d47bbace0c29145e04f5b27286ba29e"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/steschwa/fq/cmd.Version=#{version} -X github.com/steschwa/fq/cmd.CommitSHA=brew"
+    system "go", "build", *std_go_args(ldflags: ldflags)
+  end
+
+  test do
+    system "#{bin}/fq", "--help"
+  end
+end
+
