ca20a10182c282db68485ae1c63a2dedcd26264c
Stefan Schwaighofer <24265130+steschwa@users.noreply.github.com>
2025-01-26 12:29:40 +0000

    fq: add 1.2.3 bottle.

--- Formula/fq.rb
+++ Formula/fq.rb
@@ -5,6 +5,13 @@ class Fq < Formula
   sha256 "11552de7615bd092e24063c492531f998d47bbace0c29145e04f5b27286ba29e"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/steschwa/homebrew-tap/releases/download/fq-1.2.3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d0c2838fdf00f580155ff7c61d4a18f523420fa1520fe7ebde2823c64294a001"
+    sha256 cellar: :any_skip_relocation, ventura:       "363b4dca2433b1fe3b769f687fd58c20064cee03690f4a2f3f8f032d763d86d2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8116424606dc4b3fd5e9a126285ead4cc84a91ad6958f01171dcaaf513b939a8"
+  end
+
   depends_on "go" => :build
 
   def install
